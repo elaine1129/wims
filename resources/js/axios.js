3bb9424d3fd898c385279f40ebd5165b0515e8f2
elaine1129 <elainelowjingyi00@gmail.com>
2022-08-22 00:06:52 +0800

    change axios base url

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -3,8 +3,8 @@ import store from './store';
 import router from "./router";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8000/api'
-
+    // baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://fyp-wims.herokuapp.com/api'
 })
 
 axiosClient.interceptors.request.use(config => {
